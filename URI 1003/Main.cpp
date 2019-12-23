6f83075361bc15370303813170317f1f836a670b
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-22 23:47:54 -0800

    Add files via upload

--- URI 1003/Main.cpp
+++ URI 1003/Main.cpp
@@ -1,1 +1,21 @@
+// Bismillahir Rahmanir Rahim
+// URI_1003
+// SImple Sum
 
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int A, B, SOMA;
+
+	cin >> A;
+	cin >> B;
+
+	SOMA = A + B;
+	cout << "SOMA = " << SOMA;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
