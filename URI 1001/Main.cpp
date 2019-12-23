f5d7e276c3ddcfe2b4d55ab5b69f210c803b719a
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-22 23:45:58 -0800

    Add files via upload

--- URI 1001/Main.cpp
+++ URI 1001/Main.cpp
@@ -1,1 +1,23 @@
+// Bismillahir Rahmanir Rahim
+// URI_1001
+//Extremely Basic
 
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int A, B;
+
+	cin >> A;
+	cin >> B;
+
+	int X;
+
+	X = A + B;
+	cout << "X = " << X;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
