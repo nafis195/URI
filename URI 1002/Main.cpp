64cc64226ba4aa8af10e0f2f3bea6e16a1886930
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-22 23:47:04 -0800

    Add files via upload

--- URI 1002/Main.cpp
+++ URI 1002/Main.cpp
@@ -1,1 +1,24 @@
+//Bismillahir Rahmanir Rahim
+// URI_1002
+//Area of a Circle
 
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const double pi = 3.14159;
+
+int main ()
+{
+	double A, R;
+
+	cin >> R;
+	A = pi * R * R;
+	std::cout << std::fixed;
+	std::cout << std::setprecision(4);
+	std::cout << "A=" << A;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
