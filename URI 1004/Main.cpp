c1942f4d2130bd0c8e6ce24cdf37c3a5e37e73d7
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-22 23:48:45 -0800

    Add files via upload

--- URI 1004/Main.cpp
+++ URI 1004/Main.cpp
@@ -1,1 +1,20 @@
+// Bismillahir Rahmanir Rahim
+// URI_1004
+// Simple Product
 
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int a, b, PROD;
+
+	cin >> a;
+	cin >> b;
+	PROD = a * b;
+	cout << "PROD = " << PROD;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
