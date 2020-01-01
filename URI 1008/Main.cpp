b5496c4e5e846286c9090ecb4c3da3a5ae7ea140
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-31 23:05:48 -0800

    Add files via upload

--- URI 1008/Main.cpp
+++ URI 1008/Main.cpp
@@ -1,1 +1,23 @@
+// Bismillahir Raahmanir Rahim
+// URI_1008
+// Salary
 
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main ()
+{
+	int employeeNumber, workedHours;
+	float amountPerHour, salary;
+
+	cin >> employeeNumber >> workedHours >> amountPerHour;
+	salary = workedHours * amountPerHour;
+
+	cout << "NUMBER = " << employeeNumber << endl;
+	cout << std::fixed << setprecision (2) << "SALARY = U$ " << salary;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
