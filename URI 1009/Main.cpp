fa8e14cda6536e8bfab9c10e4a2c46d971ddb591
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-31 23:06:40 -0800

    Add files via upload

--- URI 1009/Main.cpp
+++ URI 1009/Main.cpp
@@ -1,1 +1,33 @@
+// Bismillahir Rahmanir Rahim
+// URI_1009
+// Salary with bonus
 
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+double totalSalary (string firstName, double salary, double salesValue)
+{
+	double total;
+	total = salary + (salesValue * .15);
+	return total;
+}
+
+int main ()
+{
+	string firstName;
+	double sellerSalary, salesTotal;
+
+	cin >> firstName;
+	cin >> sellerSalary;
+	cin >> salesTotal;
+
+	std::cout << std::fixed;
+	std::cout << std::setprecision (2);
+	cout << "TOTAL = R$ " << totalSalary (firstName, sellerSalary, salesTotal);
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
