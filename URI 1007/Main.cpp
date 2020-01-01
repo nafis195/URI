4b9a85c785071f4fe78f38b87d1ac47b3a9adad3
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-31 23:05:04 -0800

    Add files via upload

--- URI 1007/Main.cpp
+++ URI 1007/Main.cpp
@@ -1,1 +1,19 @@
+// Bismillahir Rahmanir Rahim
+// URI_1007
+// Difference
 
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int A, B, C, D, DIFERENCA;
+
+	cin >> A >> B >> C >> D;
+	DIFERENCA = (A * B - C * D);
+	cout << "DIFERENCA = " << DIFERENCA;
+
+	cout << endl;
+	system ("pause");
+	return 0;
+}
